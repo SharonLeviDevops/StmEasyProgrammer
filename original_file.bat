c7abe8c6cfcead32c98d0c8f33b2205d9e465411
Sharon.Levi <sharont55l@gmail.com>
2022-07-14 11:53:59 +0300

    add #

--- original_file.bat
+++ original_file.bat
@@ -1,12 +1,2 @@
 cls
-C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe -c port=SWD freq=4000 -e all
-echo ghgjhg
-
-rem C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\binSTM32_Programmer_CLI.exe -c port=SWD freq=4000 -e all
-rem STM32_Programmer_CLI.exe -c port=SWD freq=4000 -e all
-TIMEOUT 3
-rem C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\binSTM32_Programmer_CLI.exe -c port=SWD freq=4000 -e all
-
-C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe -c port=SWD freq=4000 -w filepath 0x08000000 paus
-pause
-C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe -c port=SWD freq=4000 -rst
+C:\"Program Files"\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe -c port=SWD mode=UR sn=002700264741500520383733 -e all --skipErase -d filepath 0x08000000 -Rst
